90b9ef9b2e2ad5cf11c299229723a4a8e2f0f335
unknown <>
2017-03-27 23:27:57 +0800

    Initial commit

---
+++ src/Mylist.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by lumia on 2017/3/24.
+ */
+public class Mylist<E extends Comparable<E>> extends ArrayList<E> {
+    public static void main(String[] args) {
+        Mylist<String> myList = new Mylist<>();
+        myList.add("A");
+        myList.add("B");
+        System.out.println(myList.inOrder());
+    }
+    public Mylist() {
+        super();
+    }
+    
+
+    public boolean inOrder() {
+        if (size() == 1 || isEmpty())
+            return true;
+        Iterator<E> iterator = iterator();
+        E lastOne = iterator.next();
+        E currentOne;
+        while (iterator.hasNext()) {
+            currentOne = iterator.next();
+            if (lastOne.compareTo(currentOne) < 0)
+                return false;
+            lastOne = currentOne;
+        }
+        return true;
+    }
+
+}
